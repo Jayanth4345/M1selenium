daddac3f9c2588cdd2f17a553b2fc499ce1f659d
DURGA <DURGA@DESKTOP-O0E0K99>
2025-02-04 12:40:46 +0530

    this is my first commit

---
+++ src/main/java/simple/github/GithubProject.java
@@ -0,0 +1,12 @@
+package simple.github;
+
+import org.testng.annotations.Test;
+
+public class GithubProject {
+    
+	
+	@Test
+	public void sample() {
+		System.out.println("pratice github");
+	}
+}
