56372f04a93d892fd5acf5a83716506844246c92
DURGA <DURGA@DESKTOP-O0E0K99>
2025-02-05 13:10:23 +0530

    Branch commit

--- src/main/java/simple/github/Gitpratice.java
+++ src/main/java/simple/github/Gitpratice.java
@@ -2,9 +2,12 @@ package simple.github;
 
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class Gitpratice {
 	@Test
 	public void m1() {
-		System.out.println("praticing pull  operation");
+		System.out.println("praticing pull operation");
+		System.out.println("Branch commit");
 	}
 }
